4ec6efc928b60698b9c2cd8b173ccc14b4b87f36
Haris Ali Mughal <harisalimughal077@gmail.com>
2026-08-06 02:48:01 +0500

    Add Script component for answero widget integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import '../src/index.css'
-import Navbar from '../src/components/Navbar'
-import Footer from '../src/components/Footer'
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import Script from "next/script";
+import "../src/index.css";
+import Navbar from "../src/components/Navbar";
+import Footer from "../src/components/Footer";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -14,7 +15,12 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <main className="overflow-x-clip">
         <Component {...pageProps} />
       </main>
+      <Script
+        src="https://api.answero.urdux.tech/widget.js"
+        data-widget-id="widget_fb6ac8f5e051e7f2396de92506b6c4cc"
+        strategy="afterInteractive"
+      />
       <Footer />
     </>
-  )
+  );
 }
